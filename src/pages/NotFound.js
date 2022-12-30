3c3285739bee2d14af1fe9e8bfb3ee1635e3c659
Jerry <59536381+Jerryc95@users.noreply.github.com>
2022-12-30 17:32:44 -0500

    update asset names

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import pixelDino from "../components/pixelDino.jpeg"
+import pixelDino from "../components/pixel-dino.jpeg"
 
 const NotFound = () => {
   return (
